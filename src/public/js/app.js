c6ab036faf4958f845c793abbfd73fb8218c9368
yang000401 <87750073+yang000401@users.noreply.github.com>
2022-04-18 14:01:41 +0900

    Add files via upload
    근데 이제 프레임워크를 사용하지 않고 해온게 아까워서 기록해두는

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,30 @@
-const socket = new WebSocket('ws://${window.location.host}')
+const messageList = document.querySelector('ul')
+const messageForm = document.querySelector('form')
+const socket = new WebSocket(`ws://${window.location.host}`)
 
 socket.addEventListener('open', () => {
-  console.log('Connected to Server')
+  function handleOpen() {
+    console.log('Connected to Server ')
+  }
+
+  socket.addEventListener('open', handleOpen)
 })
 
 socket.addEventListener('message', (message) => {
-  console.log('New message:', message.data)
+  console.log('New message: ', message.data)
 })
-
 socket.addEventListener('close', () => {
-  console.log('Disonnected frome Server')
+  console.log('Disconnected from Server ')
 })
-
 setTimeout(() => {
   socket.send('hello from the browser!')
 }, 10000)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const input = messageForm.querySelector('input')
+  socket.send(input.value)
+  input.value = ''
+}
+
+messageForm.addEventListener('submit', handleSubmit)
--- src/server.js
+++ src/server.js
@@ -1,30 +1,32 @@
 import http from 'http'
 import WebSocket from 'ws'
 import express from 'express'
-
 const app = express()
-
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
-app.get('/public', express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (_, res) => res.render('home'))
 app.get('/*', (_, res) => res.redirect('/'))
-
-const handleListen = () => console.log('listening on ws://localhost:3000')
-
+const handleListen = () => console.log(`Listening on http://localhost:3000`)
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
 
-function handleConnection(socket) {
-  console.log(socket)
+function onSocketClose() {
+  console.log('Disconnected from the Browser')
+}
+
+function onSocketMessage(message) {
+  console.log(message)
 }
 
 wss.on('connection', (socket) => {
-  console.log('Connected to Browser')
-  socket.on('close', () => console.log('Disconnected from the Browser'))
-  socekt.on('message', (message) => {
+  console.log('Connected to Browser ✅')
+  socket.on('close', () => console.log('Disconnected from the Browser ❌'))
+  socket.on('message', (message) => {
     console.log(message)
   })
+  socket.on('close', onSocketClose)
+  socket.on('message', onSocketMessage)
   socket.send('hello!!!')
 })
 
