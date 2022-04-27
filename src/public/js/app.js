d5b249f417e7389084c484c65107964a013d1c9a
yang000401 <87750073+yang000401@users.noreply.github.com>
2022-04-27 19:31:43 +0900

    중간과정?
    실시간 채팅 기능
    브라우저간의 실시간 채팅 및 socket.io ui 적용

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,52 @@
-const socket = new WebSocket('ws://${window.location.host}')
+const socket = io()
+const welcome = document.getElementById('welcome')
+const form = welcome.querySelector('form')
+const room = document.getElementById('room')
+room.hidden = true
+let roomName
+function addMessage(message) {
+  const ul = room.querySelector('ul')
+  const li = document.createElement('li')
+  li.innerText = message
+  ul.appendChild(li)
+}
+function handleMessageSubmit(event) {
+  event.preventDefault()
+  const input = room.querySelector('#msg input')
+  const value = input.value
+  socket.emit('new_message', input.value, roomName, () => {
+    addMessage(`You: ${value}`)
+  })
+  input.value = ''
+}
+function showRoom() {
+  welcome.hidden = true
+  room.hidden = false
+  const h3 = room.querySelector('h3')
+  h3.innerText = `Room ${roomName}`
+  const msgForm = room.querySelector('#msg')
+  msgForm.addEventListener('submit', handleMessageSubmit)
+}
+function handleRoomSubmit(event) {
+  event.preventDefault()
+  const input = form.querySelector('input')
+  socket.emit('enter_room', input.value, showRoom)
+  roomName = input.value
+  input.value = ''
+}
 
-socket.addEventListener('open', () => {
-  console.log('Connected to Server')
+form.addEventListener('submit', handleRoomSubmit)
+
+socket.on('welcome', (user, newCount) => {
+  const h3 = room.querySelector('h3')
+  h3.innerText = `Room ${roomName} (${newCount})`
+  addMessage(`${user} arrived!`)
 })
 
-socket.addEventListener('message', (message) => {
-  console.log('New message:', message.data)
+socket.on('bye', (left, newCount) => {
+  const h3 = room.querySelector('h3')
+  h3.innerText = `Room ${roomName} (${newCount})`
+  addMessage(`${left} left ㅠㅠ`)
 })
 
-socket.addEventListener('close', () => {
-  console.log('Disonnected frome Server')
-})
-
-setTimeout(() => {
-  socket.send('hello from the browser!')
-}, 10000)
+socket.on('new_message', addMessage)
--- src/server.js
+++ src/server.js
@@ -1,45 +1,106 @@
 import http from 'http'
-import WebSocket from 'ws'
+import SocketIO from 'socket.io'
+import { Server } from 'socket.io'
+import { instrument } from '@socket.io/admin-ui'
 import express from 'express'
 
 const app = express()
-
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
-app.get('/public', express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (_, res) => res.render('home'))
 app.get('/*', (_, res) => res.redirect('/'))
 
-const handleListen = () => console.log('listening on ws://localhost:3000')
+const httpServer = http.createServer(app)
 
-const server = http.createServer(app)
-const wss = new WebSocket.Server({ server })
-
-function handleConnection(socket) {
-  console.log(socket)
-}
-
-wss.on('connection', (socket) => {
-  console.log('Connected to Browser')
-  socket.on('close', () => console.log('Disconnected from the Browser'))
-  socekt.on('message', (message) => {
-    console.log(message)
-  })
-  socket.send('hello!!!')
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ['https://admin.socket.io'],
+    credentials: true,
+  },
 })
 
-server.listen(3000, handleListen)
+instrument(wsServer, {
+  auth: false,
+})
 
-//nodemon은 프로젝트를 살펴보고 변경사항이 있을시 서버를 재시작해주느 프로그램
-//서버를 재시작하는 대신에 babe-node를 실행하게 되는데 Babel은 우리가 작성한 코드를 일반 Node.JS코드로 컴파일 해준다
-//그 작업을 src/server.js 파일에 해주며
-//server.js 파일에서는 express를 import하고, express 어플리케이션을 구성하고, 1~3번째 줄
-//여기에 view engine을 Pug로 설정하고, views 디렉토리가 설정되며, 그리고 public 파일들에 대해서도 똑같은 작업을 해주고 있다. 5~7
-//public 파일들은 FrontEnd에서 구동되는 코드(중요) 왜 중요하냐면 여기 저기서 Js코드들을 다루다 보면 어떤게 FrontEnd고 어떤게 BackEnd인지 헷갈릴 수 있기에 주석을 담
-//그래서 이름은 app.js랑 server.js로 구분 server.js는 백엔드에서 구동될거고 app.js는 프론트엔드에서 구동된다
-//유저는 쉽게 서버 내 모든 폴더들을 들여다 볼 수 없다, 보안상 좋은일
-//지금 같은 경우에는 유저는 /public 으로 이동 할 시 public 폴더 내용을 볼 수 있다 7
-//홈페이지로 이동시 사용될 템플릿을 렌더해준다 8
-//views 폴더에 있는 home.pug를 렌더
-//express를 사용한 일반적인 NodeJS 설정임, packge.json, script 생성, babel 등등
-//babel-node를 실행시키면 babel-node는 바로 babel.config.json을 찾을테고 거기서 코드에 적용돼야 하는 preset을 실행시킨다.
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer
+  const publicRooms = []
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key)
+    }
+  })
+  return publicRooms
+}
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size
+}
+wsServer.on('connection', (socket) => {
+  socket['nickname'] = 'Anon'
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`)
+  })
+  socket.on('enter_room', (roomName, done) => {
+    socket.join(roomName)
+    done()
+    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName))
+    wsServer.sockets.emit('room_change', publicRooms())
+  })
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1)
+    )
+  })
+  socket.on('disconnect', () => {
+    wsServer.sockets.emit('room_change', publicRooms())
+  })
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`)
+    done()
+  })
+  socket.on('nickname', (nickname) => (socket['nickname'] = nickname))
+})
+/*
+const wss = new WebSocket.Server({ server });
+const sockets = [];
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+}); */
+const handleListen = () => console.log(`Listening on http://localhost:3000`)
+httpServer.listen(3000, handleListen)
+
+/*
+nodemon은 프로젝트를 살펴보고 변경사항이 있을시 서버를 재시작해주느 프로그램
+서버를 재시작하는 대신에 babe-node를 실행하게 되는데 Babel은 우리가 작성한 코드를 일반 Node.JS코드로 컴파일 해준다
+그 작업을 src/server.js 파일에 해주며
+server.js 파일에서는 express를 import하고, express 어플리케이션을 구성하고, 1~3번째 줄
+여기에 view engine을 Pug로 설정하고, views 디렉토리가 설정되며, 그리고 public 파일들에 대해서도 똑같은 작업을 해주고 있다. 5~7
+public 파일들은 FrontEnd에서 구동되는 코드(중요) 왜 중요하냐면 여기 저기서 Js코드들을 다루다 보면 어떤게 FrontEnd고 어떤게 BackEnd인지 헷갈릴 수 있기에 주석을 담
+그래서 이름은 app.js랑 server.js로 구분 server.js는 백엔드에서 구동될거고 app.js는 프론트엔드에서 구동된다
+유저는 쉽게 서버 내 모든 폴더들을 들여다 볼 수 없다, 보안상 좋은일
+지금 같은 경우에는 유저는 /public 으로 이동 할 시 public 폴더 내용을 볼 수 있다 7
+홈페이지로 이동시 사용될 템플릿을 렌더해준다 8
+views 폴더에 있는 home.pug를 렌더
+express를 사용한 일반적인 NodeJS 설정임, packge.json, script 생성, babel 등등
+babel-node를 실행시키면 babel-node는 바로 babel.config.json을 찾을테고 거기서 코드에 적용돼야 하는 preset을 실행시킨다.
+*/
